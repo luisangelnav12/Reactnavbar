2ccca9e2e8302bc934836a85d7ae4ec07eccd195
Lui <luisangelnav12@gmail.com>
2021-07-19 23:23:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header/Header";
+import Index from "./index/index";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Index />
+      
+    
+
+    </div>
+  );
+}
---
+++ src/index/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+import "./index.css";
+
+const Index = () => {
+ 
+  return (
+    <div className="holi">
+      <div className="div1" id="item1"></div>
+      <div className="div2" id="item2"></div>
+      <div className="div3" id="item3"></div>
+      <div className="div4" id="item4"></div>
+      <div className="div5" id="item5"></div>
+    </div>
+  );
+};
+
+export default Index;
