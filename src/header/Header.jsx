e4356ab5d3da4f2a9652952cbcd14cfa4138b64f
Lui <luisangelnav12@gmail.com>
2021-07-19 23:59:09 -0500

    tokio

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { ReactComponent as CloseMenu } from "../assets/x.svg";
-import { ReactComponent as MenuIcon } from "../assets/menu.svg";
-import { ReactComponent as Logo } from "../assets/logo.svg";
+// import { ReactComponent as CloseMenu } from "../assets/x.svg";
+// import { ReactComponent as MenuIcon } from "../assets/menu.svg";
+// import { ReactComponent as Logo } from "../assets/logo.svg";
 import "./header.css";
+const x = "../assets/x.svg";
+const menu = "../assets/menu.svg";
+const logo = "../assets/logo.png";
 
 const Header = () => {
   const [click, setClick] = useState(false);
@@ -13,30 +16,41 @@ const Header = () => {
       <div className="logo-nav">
         <div className="logo-container">
           <a href="#">
-            <Logo className="logo" />
+            <img src={logo} className="logo" />
           </a>
         </div>
         <ul className={click ? "nav-options active" : "nav-options"}>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="/#item1">ABOUT</a>
+            <a href="/#item1">Inicio</a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="/#item2">CONTACT</a>
+            <a href="/#item2">Nosotros</a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="/#item3">BLOG</a>
+            <a href="/#item3">Servicios</a>
           </li>
-          <li className="option mobile-option" onClick={closeMobileMenu}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="/#item3">Contacto</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="/#item3">Unete al equipo</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="/#item3">Aula virtual</a>
+          </li>
+
+          {/* <li className="option mobile-option" onClick={closeMobileMenu}>
             <a href="#">SIGN-IN</a>
-          </li>
+          </li> 
           <li className="option mobile-option" onClick={closeMobileMenu}>
             <a href="" className="sign-up">
               SIGN-UP
             </a>
-          </li>
+          </li>*/}
+       
         </ul>
       </div>
-      <ul className="signin-up">
+      {/* <ul className="signin-up">
         <li className="sign-in" onClick={closeMobileMenu}>
           <a href="#">SIGN-IN</a>
         </li>
@@ -45,12 +59,12 @@ const Header = () => {
             SIGN-UP
           </a>
         </li>
-      </ul>
+      </ul> */}
       <div className="mobile-menu" onClick={handleClick}>
         {click ? (
-          <CloseMenu className="menu-icon" />
+          <img src={x} className="menu-icon" />
         ) : (
-          <MenuIcon className="menu-icon" />
+          <img src={menu} className="menu-icon" />
         )}
       </div>
     </div>
